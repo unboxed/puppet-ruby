78137c61c2c7ed63d77c2fcbcb8b8b85cdc759e0
Sven Agnew <sven@kangohammer.com>
2013-11-22 11:01:02 +0200

    Fix broken target for contiki ree

--- manifests/ree.pp
+++ manifests/ree.pp
@@ -11,6 +11,6 @@ class ruby::ree {
   file { "${ruby::rbenv_root}/versions/ree":
     ensure => symlink,
     force  => true,
-    target => "${ruby::rbenv_root}/versions/ree-1.8.7-2012.02"
+    target => "${ruby::rbenv_root}/versions/ree-1.8.7-2011.03"
   }
 }
